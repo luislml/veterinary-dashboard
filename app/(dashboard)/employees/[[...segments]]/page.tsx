7e62f1fc6170d74fde3e256acf3d2029ccd1504a
macbook pro <macbookpro@Edson.local>
2025-08-14 22:36:13 -0400

    init proyect

---
+++ app/(dashboard)/employees/[[...segments]]/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+import * as React from 'react';
+
+import { useParams } from 'next/navigation';
+
+import { Crud } from '@toolpad/core/Crud';
+import { employeesDataSource, Employee, employeesCache } from '../../../../data/employees';
+
+
+export default function EmployeesCrudPage() {
+  
+  const params = useParams();
+  const [employeeId] = params.segments ?? [];
+  
+  
+  
+
+  return (
+    <Crud<Employee>
+      dataSource={employeesDataSource}
+      dataSourceCache={employeesCache}
+      rootPath="/employees"
+      initialPageSize={25}
+      defaultValues={{ itemCount: 1 }}
+      pageTitles={{
+        show: `Employee ${employeeId}`,
+        create: 'New Employee',
+        edit: `Employee ${employeeId} - Edit`,
+      }}
+    />
+  );
+}
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import { PageContainer } from '@toolpad/core/PageContainer';
+import { auth } from '../../auth';
+
+export default async function HomePage() {
+  const session = await auth();
+
+  return (    
+    <PageContainer>
+      <Typography>
+        Welcome to Toolpad, {session?.user?.name || 'User'}!
+      </Typography>
+    </PageContainer>
+  );
+}
